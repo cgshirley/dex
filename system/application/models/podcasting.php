3d0319fa4a8f3bf2ecb7a79c9e0f4f2f78fd18fd
Sherwin Soltani <sherwin@wybc.com>
2010-08-16 19:54:06 -0400

    initial work on podcasting.php with functions for retrieving podcast data by id, member_id, title, and podcast entries by the podcast id

---
+++ system/application/models/podcasting.php
@@ -0,0 +1,44 @@
+<?php
+
+class Podcasting extends Model {
+
+    function Podcasting() {
+        parent::Model();
+    }
+
+    // YOU MUST CONSTRUCT ADDITIONAL LIST PROCESSING :(
+    // this function can only handle DB results, instead of
+    // an arbitrary iterator, like LISP can.
+    static function collect($result) {
+        foreach ($result->result_array() as $row) {
+            $retval[] = $row;
+        }
+        return $retval;
+    }
+
+    function get_podcast($id) {
+        $sql = "SELECT (id, member_id, title, subtitle, artist, copyright, link, image, description) FROM podcasts WHERE id = ?;";
+        $result = $this->db->query($sql, array($id));
+        return $result->row_array();
+    }
+
+    function get_member_podcasts($member_id) {
+        $sql = "SELECT * FROM podcasts WHERE member_id = ?;";
+        $result = $this->db->query($sql, array($id));
+        return collect($result);
+    }
+
+    function get_podcasts_titled($title) {
+        $sql = "SELECT * FROM podcasts WHERE title = ?;";
+        $result = $this->db->query($sql, array($title));
+        return collect($result);
+    }
+
+    function get_podcast_entries($id) {
+        $sql = "SELECT * FROM podcast_entries WHERE id = ?;";
+        $result = $this->db->query($sql, array($title));
+        return collect($result);
+    }
+
+}
+?>
