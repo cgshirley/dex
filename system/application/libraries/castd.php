10c99abc49d72939d18ded69f6bc9ea46bd1df66
Sherwin Soltani <sherwin@wybc.com>
2010-08-20 01:24:01 -0400

    moved code from the model to library for interfacing with the castd (podcasting daemon), which is how WYBC will interface with its podcasting system.

---
+++ system/application/libraries/castd.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Interface to castd, the WYBC podcasting daemon.
+ * @package Castd
+ * @author Sherwin Soltani
+ * @version 0.1
+ */
+
+class Castd {
+
+    function export_directives($podcast, $entries) {
+        $podcast = get_podcast($podcast_id);
+        $entries = get_podcast_entries($podcast_id);
+        $basestr = "channel\n";
+        $basestr .= directive("title", $podcast['title']);
+        $basestr .= directive("subtitle", $podcast['subtitle']);
+        $basestr .= directive("description", str_replace("\n", "", $podcast['description']));
+        $basestr .= directive("copyright", $podcast['copyright']);
+        $basestr .= directive("language", $podcast['language']);
+        $basestr .= directive("image", $podcast['image']);
+        $basestr .= directive("link", $podcast['link']);
+        foreach ($entries as $item) {
+            $basestr .= "item\n";
+            $basestr .= directive("title", $item['title']);
+            $basestr .= directive("subtitle", $item['subtitle']);
+            $basestr .= directive("description", $item['description']);
+            $basestr .= directive("guid", $item['guid']);
+            $basestr .= "file " . $item['file_size'] .  " " . $item['file_link'];
+            $basestr .= directive("duration", $item['duration']);
+            $basestr .= directive("published", $item['timestamp']);
+            $basestr .= directive("keywords", $item['keywords']);
+        }
+        return $basestr;
+    }
+}
+?>
